dfed1fb0dba6f59d5284314f9710f6da76eba48d
Jared Grimes <jaredlgrimes@hotmail.com>
2022-07-26 20:59:39 -0400

    removed link underline

--- src/common/Link.tsx
+++ src/common/Link.tsx
@@ -1,5 +1,10 @@
 import MuiLink, { LinkProps } from '@mui/material/Link';
 
 export const Link = (props: LinkProps) => (
-  <MuiLink {...props} target='_blank' rel='noreferrer' />
+  <MuiLink
+    {...props}
+    target='_blank'
+    rel='noreferrer'
+    sx={{ textDecoration: 'none' }}
+  />
 );
