505f175fd254a6c3fb9f22357ea57a38c7334a96
Jared Grimes <jaredlgrimes@hotmail.com>
2022-07-28 15:48:34 -0400

    sentry io

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,18 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import { App } from './App';
 import reportWebVitals from './reportWebVitals';
+import * as Sentry from "@sentry/react";
+import { BrowserTracing } from "@sentry/tracing";
+
+Sentry.init({
+  dsn: "https://159a318ad2d94af3980ff8d8d5df8cd8@o1338377.ingest.sentry.io/6609243",
+  integrations: [new BrowserTracing()],
+
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  // We recommend adjusting this value in production
+  tracesSampleRate: 1.0,
+});
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
