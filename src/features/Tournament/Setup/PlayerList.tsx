5a7ae29656dc606ec8b6d96c734fb271b983ba8e
Jared Grimes <jaredlgrimes@hotmail.com>
2022-09-07 23:21:19 -0400

    refactor list item component

--- src/features/Tournament/Setup/PlayerList.tsx
+++ src/features/Tournament/Setup/PlayerList.tsx
@@ -1,96 +1,12 @@
 import List from '@mui/material/List';
-import ListItem from '@mui/material/ListItem';
-import ListItemButton from '@mui/material/ListItemButton';
 import { Player } from '../Player/types';
 import Card from '@mui/material/Card';
-import ListItemText from '@mui/material/ListItemText';
-import IconButton from '@mui/material/IconButton';
-import EditIcon from '@mui/icons-material/Edit';
-import SaveIcon from '@mui/icons-material/Save';
-import Input from '@mui/material/Input';
-import React from 'react';
-import styled from '@emotion/styled';
+import { PlayerListItem } from './PlayerListItem';
 
 interface PlayerListProps {
   players: Player[];
   updatePlayerName: (playerId: string, newName: string) => void;
 }
-
-const PlayerListCardContent = styled(Card)`
-  padding: 0;
-`;
-
-const PrettyIcon = styled.div`
-  opacity: 0.54;
-`;
-
-interface PlayerListItemProps {
-  player: Player;
-  idx: number;
-  updatePlayerName: (playerId: string, newName: string) => void;
-}
-
-const PlayerNameInput = styled(Input)`
-  margin: 8px 16px;
-`;
-
-export const PlayerListItem = (props: PlayerListItemProps) => {
-  const [isHovering, setIsHovering] = React.useState(false);
-  const [isEditing, setIsEditing] = React.useState(false);
-  const [playerNameInput, setPlayerNameInput] = React.useState<string>(
-    props.player.name
-  );
-
-  const commitPlayerEdit = () => {
-    setIsEditing(false);
-    props.updatePlayerName(props.player.id, playerNameInput);
-  };
-
-  if (isEditing) {
-    return (
-      <ListItem
-        key={props.idx}
-        disablePadding
-        secondaryAction={
-          <IconButton onClick={() => commitPlayerEdit()}>
-            <SaveIcon />
-          </IconButton>
-        }
-      >
-        <PlayerNameInput
-          value={playerNameInput}
-          onChange={e => setPlayerNameInput(e.target.value)}
-          onKeyPress={e => {
-            if (e.key === 'Enter') {
-              commitPlayerEdit();
-            }
-          }}
-          autoFocus
-        />
-      </ListItem>
-    );
-  }
-
-  return (
-    <ListItem
-      key={props.idx}
-      disablePadding
-      secondaryAction={
-        isHovering && (
-          <IconButton onClick={() => setIsEditing(true)}>
-            <EditIcon />
-          </IconButton>
-        )
-      }
-      onMouseEnter={() => setIsHovering(true)}
-      onMouseLeave={() => setIsHovering(false)}
-    >
-      <ListItemButton disableRipple onClick={() => setIsEditing(true)}>
-        <ListItemText>{props.player.name}</ListItemText>
-      </ListItemButton>
-    </ListItem>
-  );
-};
 
 export const PlayerList = (props: PlayerListProps) => {
   return (
---
+++ src/features/Tournament/Setup/PlayerListItem.tsx
@@ -0,0 +1,78 @@
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import { Player } from '../Player/types';
+import ListItemText from '@mui/material/ListItemText';
+import IconButton from '@mui/material/IconButton';
+import EditIcon from '@mui/icons-material/Edit';
+import SaveIcon from '@mui/icons-material/Save';
+import Input from '@mui/material/Input';
+import React from 'react';
+import styled from '@emotion/styled';
+
+const PlayerNameInput = styled(Input)`
+  margin: 8px 16px;
+`;
+
+interface PlayerListItemProps {
+  player: Player;
+  idx: number;
+  updatePlayerName: (playerId: string, newName: string) => void;
+}
+
+export const PlayerListItem = (props: PlayerListItemProps) => {
+  const [isHovering, setIsHovering] = React.useState(false);
+  const [isEditing, setIsEditing] = React.useState(false);
+  const [playerNameInput, setPlayerNameInput] = React.useState<string>(
+    props.player.name
+  );
+
+  const commitPlayerEdit = () => {
+    setIsEditing(false);
+    props.updatePlayerName(props.player.id, playerNameInput);
+  };
+
+  if (isEditing) {
+    return (
+      <ListItem
+        key={props.idx}
+        disablePadding
+        secondaryAction={
+          <IconButton onClick={() => commitPlayerEdit()}>
+            <SaveIcon />
+          </IconButton>
+        }
+      >
+        <PlayerNameInput
+          value={playerNameInput}
+          onChange={e => setPlayerNameInput(e.target.value)}
+          onKeyPress={e => {
+            if (e.key === 'Enter') {
+              commitPlayerEdit();
+            }
+          }}
+          autoFocus
+        />
+      </ListItem>
+    );
+  }
+
+  return (
+    <ListItem
+      key={props.idx}
+      disablePadding
+      secondaryAction={
+        isHovering && (
+          <IconButton onClick={() => setIsEditing(true)}>
+            <EditIcon />
+          </IconButton>
+        )
+      }
+      onMouseEnter={() => setIsHovering(true)}
+      onMouseLeave={() => setIsHovering(false)}
+    >
+      <ListItemButton disableRipple onClick={() => setIsEditing(true)}>
+        <ListItemText>{props.player.name}</ListItemText>
+      </ListItemButton>
+    </ListItem>
+  );
+};
